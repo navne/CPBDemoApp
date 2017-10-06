004fbcab9776aedae38abbf2d5d1ef9fc8e746d6
Sada <Sada@192.168.0.3>
2017-10-07 00:47:53 +0530

    InitialCommits

---
+++ src/main/java/com/sada/CPBCITAApplication.java
@@ -0,0 +1,12 @@
+package com.sada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CPBCITAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CPBCITAApplication.class, args);
+	}
+}
